0b5d9ee872a00119f6a9f92b5cec412458cc93de
nohreit <hierno.mah@gmail.com>
2022-05-19 17:04:51 -0400

    update

--- java/src/prog_1/Main.java
+++ java/src/prog_1/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 //        Methods.run();
 //        YardsFeetInches.run();
-        Grades.run();
+//        Grades.run();
     }
 
 }
